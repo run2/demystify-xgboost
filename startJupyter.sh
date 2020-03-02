b736d8ed0c7c153dde1de3e5353fae38495d6580
run2 <b.debanjan@gmail.com>
2020-03-02 18:36:03 +0530

    Add files via upload
    Entry point for container

---
+++ startJupyter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo -H -u ml-user -- sh -c '. /home/ml-user/.bashrc ; \
+cd ; pwd ; chown ml-user ml-disk ; chgrp ml-user ml-disk ; \
+PATH=$PATH:/home/ml-user/.local/bin:/home/ml-user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games; \
+export PATH; jupyter lab &'
+bash
